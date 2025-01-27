cec3754d1bb8df3769135ddd0471857e96c97a36
Troldefar <r_emil@live.dk>
2025-01-27 09:14:17 +0100

    update

--- zebra.js
+++ zebra.js
@@ -1,20 +1,22 @@
 class Zebra {
 
-    constructor(value) {
-        this.customValue = value;
-    }
-
-    ZEBRA_MAX_STRING_BUFFER_LENGTH = 500;
-    ZEBRA_DEFAULT_LABEL_LENGTH = 200;
-    serviceUUID = 'your-service-uuid';
-    characteristicUUID = 'your-characteristic-uuid';
-
-    namePrefix = 'xxxxxx';
-
     // ZPL Commands we might need
     deviceLanguageZPL = '! U1 setvar "device.languages" "zpl"';
     deviceLanguageLinePrint = '! U1 setvar "device.languages" "line_print"';
-    resetDevice = '! U1 do "device.reset" "" <CR>';
+    resetDevice = '! U1 do "device.reset" ""';
+    setReconnect = '! U1 setvar "bluetooth.enable_reconnect" "off"'; 
+    discoverableOn = '! U1 setvar "bluetooth.discoverable" "on"';
+    minimumSecurityMode = '! U1 setvar "bluetooth.minimum_security_mode" "1"';
+    ping = 'ping';
+    resetBuffer = '~JA';
+
+    serviceUUID = 'xxxxx';
+    characteristicUUID = 'xxxxx';
+    
+    ZEBRA_MAX_STRING_BUFFER_LENGTH = 250;
+    ZEBRA_DEFAULT_LABEL_LENGTH = 600;
+
+    namePrefix = 'xxxxxxx';
 
     /**
      * Dummy getter for POC
@@ -23,50 +25,31 @@ class Zebra {
      * @returns string
      */
 
-    getZPLCommand() {
+    header() {
         return `
             ^XA
-    
-            ^LT40
-            ^FX Top section with logo, name and address.
-            ^CF0,60
-            ^FO50,50^GB100,100,100^FS
-            ^FO75,75^FR^GB100,100,100^FS
-            ^FO93,93^GB40,40,40^FS
-            ^FO220,50^FD${this.customValue}, Inc.^FS
-            ^CF0,30
-    
-            ^FO220,115^FD1000 Shipping Lane^FS
-            ^FO220,155^FDShelbyville TN 38102^FS
-            ^FO220,195^FDUnited States (USA)^FS
-            ^FO50,250^GB700,3,3^FS
-    
-            ^FX Second section with recipient address and permit information.
-            ^CFA,30
-            ^FO50,300^FDJohn Doe^FS
-            ^FO50,340^FD100 Main Street^FS
-            ^FO50,380^FDSpringfield TN 39021^FS
-            ^FO50,420^FDUnited States (USA)^FS
-            ^CFA,15
-            ^FO600,300^GB150,150,3^FS
-            ^FO638,340^FDPermit^FS
-            ^FO638,390^FD123456^FS
-            ^FO50,500^GB700,3,3^FS
-    
-            ^FX Third section with bar code.
-            ^BY5,2,270
-            ^FO100,550^BC^FD12345678^FS
-    
-            ^FX Fourth section (the two boxes on the bottom).
-            ^FO50,900^GB700,250,3^FS
-            ^FO400,900^GB3,250,3^FS
+
+            ^LL300
+
+            ^FX Label header
             ^CF0,40
-            ^FO100,960^FDCtr. X34B-1^FS
-            ^FO100,1010^FDREF1 F00B47^FS
-            ^FO100,1060^FDREF2 BL4H8^FS
-            ^CF0,190
-            ^FO470,955^FDCA^FS
-    
+            ^FO260,75^FHello world header^FS
+
+            ^XZ
+        `;
+    }
+
+    getZPLCommand(car, includeHeader = true) {
+        const booking = JSON.parse(car.Booking);
+
+        return `
+            ^XA
+
+            ^LL700
+
+            ^CFA,25
+            ^FO50,70^FDHello world body^FS
+
             ^XZ
         `;
     }
